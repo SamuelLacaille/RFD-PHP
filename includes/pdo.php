ead475806724546cde25c5e955b704a1c53d1b44
Cyr LIYELLY <17665@csmrouen.net>
2019-02-20 16:47:26 +0100

    CSS V1.0.0

--- includes/pdo.php
+++ includes/pdo.php
@@ -3,7 +3,7 @@
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=rfd-php',
         'root',
-        '',
+        'root',
         array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
