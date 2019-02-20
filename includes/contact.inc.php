e5b425fa976e19b2f80ae1245f3579d5da941e60
Samuel LACAILLE <samuel76560@outlook.fr>
2019-02-20 12:02:38 +0100

    html de la page de contact

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -1,1 +1,45 @@
+<?php
+if(!isset($nom)) $nom = "";
+if(!isset($prenom)) $prenom = "";
+if(!isset($mail)) $mail = "";
+if(!isset($commentaire)) $commentaire = "";
+?>
+
+
+
 <h1>Contact</h1>
+
+<form method="post" action="index.php?page=login">
+
+
+
+    <div>
+        <label for="nom">Nom&nbsp;: </label>
+        <input type="text" id="nom" name="nom" value="<?=$nom?>" />
+    </div>
+    <div>
+        <label for="prenom">Prenom&nbsp;: </label>
+        <input type="text" id="prenom" name="prenom" value="<?=$prenom?>" />
+    </div>
+    <div>
+        <label for="mail">Adresse mail&nbsp;: </label>
+        <input type="text" id="mail" name="mail" value="<?=$mail?>" />
+    </div>
+
+    <div>
+        <label for="commentaire">Laissez nous un commentaire</label>
+
+        <textarea id="commentaire" name="commentaire" value="<?=$commentaire?>"
+                  rows="5" cols="33">
+
+        </textarea>
+    </div>
+
+
+
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Envoyer" />
+    </div>
+    <input type="hidden" name="barnabe" />
+</form>
